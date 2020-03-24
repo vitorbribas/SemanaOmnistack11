dafd6307e50bf2861bcf2009a100bcad6de01865
Vítor <bsbribas66@gmail.com>
2020-03-24 20:45:51 -0300

    install nodemon dependency

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (request, response) => {
+app.get('/users', (request, response) => {
   return response.json({ 
     evento: 'Semana Omnistack',
     aluno: 'Vítor Ribas'
