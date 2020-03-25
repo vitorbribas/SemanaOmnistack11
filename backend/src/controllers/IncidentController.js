9f2eace7dc477f6c84e004292ede5e4bdf1fc039
Vítor <bsbribas66@gmail.com>
2020-03-24 23:57:47 -0300

    add ongs attributes to incidents index response

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -16,9 +16,11 @@ module.exports = {
     const [total_incidents] = await connection('incidents').count();
 
     const incidents = await connection('incidents')
+      .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
       .limit(5)
       .offset((page - 1) * 5)
-      .select('*');
+      .select(['incidents.*',
+        'ongs.name', 'ongs.email', 'ongs.whatsapp', 'ongs.city', 'ongs.uf']);  // ongs attributes
 
     response.header('X-Total-Count', total_incidents['count(*)']);
     response.json(incidents);
