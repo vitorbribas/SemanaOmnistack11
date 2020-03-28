7e539d7668b318dabfeb217b41ce870e044b76ec
Vítor <bsbribas66@gmail.com>
2020-03-27 22:36:57 -0300

    add new incident page

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Login from './pages/Login';
 import Register from './pages/Register';
 import OngProfile from './pages/OngProfile';
+import NewIncident from './pages/NewIncident';
 
 export default function Routes(params) {
   return (
@@ -12,6 +13,7 @@ export default function Routes(params) {
         <Route exact path="/" component={Login} />
         <Route path="/register" component={Register} />
         <Route path="/ong-profile" component={OngProfile} />
+        <Route path="/incidents/new" component={NewIncident} />
       </Switch>
     </BrowserRouter>
   );
