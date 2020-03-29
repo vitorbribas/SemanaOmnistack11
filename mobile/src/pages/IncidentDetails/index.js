ee9e85c5d34c3bd2a33f513d40f7c371ae377f7d
Vítor <bsbribas66@gmail.com>
2020-03-29 19:08:42 -0300

    add incident details page

--- mobile/src/pages/IncidentDetails/index.js
+++ mobile/src/pages/IncidentDetails/index.js
@@ -1,8 +1,56 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+import { Feather } from '@expo/vector-icons';
+
+import styles from './styles';
+import logoImg from '../../assets/logo.png';
 
 export default function IncidentDetails() {
+  const navigation = useNavigation();
+
+  function navigationBack() {
+    navigation.goBack();
+  }
+
   return (
-    <View />
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={logoImg} />
+
+        <TouchableOpacity onPress={navigationBack}>
+          <Feather name="arrow-left" size={28} color="#e82041" />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.incident}>
+        <Text style={[styles.incidentProperty, { marginTop: 0 }]}> ONG: </Text>
+        <Text style={styles.incidentValue }> APD </Text>
+
+        <Text style={styles.incidentProperty}> CASO: </Text>
+        <Text style={styles.incidentValue }> Poluição sonora </Text>
+
+        <Text style={styles.incidentProperty}> Valor: </Text>
+        <Text style={styles.incidentValue }> R$ 120,00 </Text>
+      </View>
+
+      <View style={styles.contactBox}>
+        <Text style={styles.heroTitle}>Salve o dia!</Text>
+        <Text style={styles.heroTitle}>Seja o herói desse caso.</Text>
+
+        <Text style={styles.heroDescription}>Entre em contato</Text>
+
+        <View style={styles.actions}>
+          <TouchableOpacity style={styles.action} onPress={() => {}}>
+            <Text style={styles.actionText}> Whatsapp </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.action} onPress={() => {}}>
+            <Text style={styles.actionText}> Email </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
   );
 }
