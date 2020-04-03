c5ae144a391e5dff3b854043d537c843d639a47f
Vítor <bsbribas66@gmail.com>
2020-04-03 00:02:55 -0300

    add type and presence validations

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { celebrate, Joi, Segments } = require('celebrate');
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const OngProfileController = require('./controllers/OngProfileController');
@@ -7,13 +8,45 @@ const SessionController = require('./controllers/SessionController');
 routes = express.Router();
 
 routes.get('/ongs',  OngController.index);
-routes.post('/ongs', OngController.create);
 
-routes.post('/incidents',       IncidentController.create);
-routes.get('/incidents',        IncidentController.index);
-routes.delete('/incidents/:id', IncidentController.destroy);
+routes.post('/ongs', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
+    whatsapp: Joi.string().required().min(10).max(11),
+    city: Joi.string().required(),
+    uf: Joi.string().required().length(2)
+  })
+}), OngController.create);
 
-routes.get('/profile/incidents', OngProfileController.index);
+routes.post('/incidents', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required()
+  }).unknown(),
+  [Segments.BODY]: Joi.object().keys({
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    value: Joi.number().required(),
+  })
+}),IncidentController.create);
+
+routes.get('/incidents', celebrate({
+  [Segments.QUERY]: Joi.object().keys({
+    page: Joi.number()
+  })
+}), IncidentController.index);
+
+routes.delete('/incidents/:id', celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.number().required()
+  })
+}), IncidentController.destroy);
+
+routes.get('/profile/incidents', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required()
+  }).unknown()
+}), OngProfileController.index);
 
 routes.post('/login', SessionController.create);
 
