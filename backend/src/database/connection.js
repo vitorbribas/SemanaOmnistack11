a9664d8714d3e5c9a35093e39e1fcf526e6257aa
Vítor <bsbribas66@gmail.com>
2020-04-05 14:45:27 -0300

    configure database for running integration tests

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,5 +1,6 @@
 const knex = require('knex');
 const databaseConfiguration = require('../../knexfile');
-const connection = knex(databaseConfiguration.development);
+const config = process.env.NODE_ENV === 'test' ? databaseConfiguration.test : databaseConfiguration.development;
+const connection = knex(config);
 
 module.exports = connection;
